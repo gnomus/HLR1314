16ff4ef0391dbd1e80e58e2f5cf8bb065301c24b
Felix Favre <1favre@informatik.uni-hamburg.de>
2013-11-21 13:14:26 +0100

    sldkfj

--- Aufgaben/blatt6/NissleyFavre/timempi.c
+++ Aufgaben/blatt6/NissleyFavre/timempi.c
@@ -1,60 +1,106 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
 #include <unistd.h>
 #include <sys/time.h>
+#include <malloc.h>
 
-int main(int argc, char const **argv)
+const int HOSTNAME_WITH_TIME_MESSAGE_TAG= 42;
+const int HOSTNAME_LENGTH = 128;
+const int MASTER_PROCESS = 0;
+
+struct hostname_with_time {
+  struct timeval tv;
+  char hostname[128];
+};
+
+int main(int argc, char **argv)
 {
 	//Initialisiere die MPI-Umgebung mit den Parametern der Funktion
-	MPI_Init(&argc, &argv);
+  MPI_Init(&argc, &argv);
 
-	//Anzahl der Prozesse
-	int world_size;
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  //Let's see how many Processes we got
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-	//Rang des Prozesses
-  	int world_rank;
-  	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  //And what Process we are
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-  	//Name des Prozessors
-  	char processor_name[MPI_MAX_PROCESSOR_NAME];
-  	int name_len;
-  	MPI_Get_processor_name(processor_name, &name_len);
+  //Single Process? Boring!
+  if (world_size < 2) {
+    fprintf(stderr,"Requires at least two processes.\n");
+    exit(-1);
+  }
 
+  //The MPI Datatypes to declare
+  MPI_Datatype MPI_HOSTNAME_WITH_TIME, MPI_HOSTNAME, MPI_TIMEVAL;
 
-  	//Prozess 1-n erzeugen HOSTNAME: TIMESTAMP als string und
-  	//übergeben diesen an Prozess 0
-  	if (world_rank != 0)
-  	{
-      char hostname[128];
-      gethostname(hostname, sizeof hostname);
+  //Hostname Type
+  MPI_Type_contiguous(HOSTNAME_LENGTH, MPI_CHAR, &MPI_HOSTNAME);
 
- 		char *timestamp = gettimeofday();
- 		char *ausgabe = hostname + ":" + timestamp;
+  //Timeval Type
+  int timeval_blocklengths[2] = {4,4};
+  MPI_Datatype timeval_subtypes[2] = {MPI_LONG, MPI_LONG};
+  MPI_Aint timeval_offsets[2];
 
- 		MPI_Send((void *) ausgabe, int count, MPI_Datatype datatype, 0, int tag, MPI_Comm communicator)
-	}
+  timeval_offsets[0] = offsetof(struct timeval, tv_sec);
+  timeval_offsets[1] = offsetof(struct timeval, tv_usec);
+
+  MPI_Type_struct(1, timeval_blocklengths, timeval_offsets, timeval_subtypes, &MPI_TIMEVAL);
+  MPI_Type_commit(&MPI_TIMEVAL);
+
+  //Hostname with Time Type
+  int hostname_with_time_blocklengths[2] = {8, 128};
+  MPI_Datatype hostname_with_time_subtypes[2] = {MPI_TIMEVAL, MPI_HOSTNAME};
+  MPI_Aint hostname_with_time_offsets[2];
+
+  hostname_with_time_offsets[0] = offsetof(struct hostname_with_time, tv);
+  hostname_with_time_offsets[1] = offsetof(struct hostname_with_time, hostname);
+
+  MPI_Type_struct(1, hostname_with_time_blocklengths, hostname_with_time_offsets, hostname_with_time_subtypes, &MPI_HOSTNAME_WITH_TIME);
+  MPI_Type_commit(&MPI_HOSTNAME_WITH_TIME);
+
+  if (world_rank != MASTER_PROCESS)
+  {
+    sleep(1);
+
+    struct hostname_with_time *p = (struct hostname_with_time*) malloc(sizeof (struct hostname_with_time));
+
+    gethostname(p->hostname, sizeof(p->hostname));
+
+    gettimeofday(&(p->tv), NULL);
+
+    printf("%d Sending now\n",world_rank);
+    MPI_Send((void *) &p, 1, MPI_HOSTNAME_WITH_TIME, MASTER_PROCESS, HOSTNAME_WITH_TIME_MESSAGE_TAG, MPI_COMM_WORLD);
+  }
 
  	//Prozess 0 gibt den String aus
   	//Nach Rang der Prozesse geordnet
- 	if (world_rank == 0)
- 	{
- 		for (int i = 0; i < world_size; ++i)
- 		{
-			MPI_Recv(ausgabe, int count, MPI_Datatype datatype, i, int tag, MPI_Comm communicator, MPI_Status* status);
- 			printf("%s:%s\n",(char *) ausgabe);
- 		}
-	}
+  if (world_rank == MASTER_PROCESS)
+  {
+   for (int i = 1; i < world_size; ++i)
+   {
+    void *buf = malloc(sizeof(struct hostname_with_time));
+    printf("%d Waiting for %d\n", world_rank,i);
+    //MPI_Status *status;
+    MPI_Recv(buf, 1, MPI_HOSTNAME_WITH_TIME, i, HOSTNAME_WITH_TIME_MESSAGE_TAG, MPI_COMM_WORLD, NULL);
+    printf("%d Bla2\n", world_rank);
+    struct hostname_with_time *message = (struct hostname_with_time*) buf;
+    printf("%s\n", message->hostname);
+  }
+}
 
-  	//Prozesse beenden erst wenn alle Ausgaben fertig sind
-
-  	MPI_Barrier(MPI_COMM_WORLD);
+  //Prozesse beenden erst wenn alle Ausgaben fertig sind
+printf("Rang %d waiting at Barrier\n", world_rank);
+  MPI_Barrier(MPI_COMM_WORLD);
+printf("Rang %d hopped over Barrier\n", world_rank);
 
   	//Rang X beendet jetzt!
-  	printf("Rang %d beendet jetzt\n", world_rank);
+printf("Rang %d beendet jetzt\n", world_rank);
 
   	//Beende die MPI-Umgebung
-  	MPI_Finalize();
+    MPI_Finalize();
 
-	return 0;
+    return 0;
 }
